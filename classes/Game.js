9b51f2da464c04942bff1c8543660f0deabf89ea
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-03-06 08:35:26 +0700

    initial commit

---
+++ classes/Game.js
@@ -0,0 +1,21 @@
+class Game {
+  players = [];
+  points = []
+  
+  constructor(players) {
+    this.players = players;
+    
+  }
+  
+  put(players, card) {
+    
+  }
+  
+  draw(players, amount) {
+    
+  }
+  
+  endTurn() {
+    
+  }
+}
---
+++ classes/Player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor() {
+    
+  }
+
+  put() {
+    
+  }
+
+  draw() {
+    
+  }
+}
