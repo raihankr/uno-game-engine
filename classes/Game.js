21d8380978e48889f400fd88a68d46d71ded1507
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-03-06 15:09:01 +0700

    make changes

--- classes/Game.js
+++ classes/Game.js
@@ -1,21 +1,32 @@
 class Game {
-  players = [];
-  points = []
-  
+  #players = [];
+  #points = new Map();
+
   constructor(players) {
-    this.players = players;
-    
+    this.#players = players;
   }
-  
+
+  newGame() {
+
+  }
+
   put(players, card) {
-    
+
   }
-  
+
   draw(players, amount) {
-    
+
   }
-  
+
   endTurn() {
-    
+
+  }
+
+  getPoints(player) {
+    return this.#points[player];
+  }
+
+  get players() {
+    return this.#players;
   }
 }
