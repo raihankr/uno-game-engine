cdf47a9f1b9057caae8e48d2036fd0d878cf2a31
Raihan Khairul Rochman <rkhairulrochman912@gmail.com>
2024-03-15 20:51:11 +0700

    Add ESLint configuration file

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+
+
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+    'mocha': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'plugins': [
+    '@stylistic/js'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    '@stylistic/js/indent': [
+      'warn',
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    '@stylistic/js/linebreak-style': [
+      'warn',
+      'windows'
+    ],
+    '@stylistic/js/semi': [
+      'warn',
+      'always'
+    ],
+    '@stylistic/js/max-len': [
+      'warn',
+      {
+        'code': 80,
+      }
+    ]
+  }
+};
